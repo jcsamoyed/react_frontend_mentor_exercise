65da6a34a2b78ca118ac3027a883497105508747
jcsamoyed <jcsamoyed@gmail.com>
2025-12-30 18:17:20 +0800

    feat(BlogPreviewCard): RWD

--- app/blog-preview-card/page.tsx
+++ app/blog-preview-card/page.tsx
@@ -14,7 +14,7 @@ export default function BlogPreviewCard() {
         'min-h-screen bg-yellow-300 flex items-center justify-center'
       )}
     >
-      <div className='flex flex-col gap-6 p-6 bg-white rounded-[20px] border border-gray-950 max-w-81.75 shadow-[8px_8px_black]'>
+      <div className='flex flex-col gap-6 p-6 bg-white rounded-[20px] border border-gray-950 max-w-81.75 lg:max-w-[384px] mx-6 shadow-[8px_8px_black]'>
         <picture className='block rounded-[10px] overflow-hidden h-50'>
           <Image
             src={illustrationArticle}
@@ -23,16 +23,16 @@ export default function BlogPreviewCard() {
           />
         </picture>
         <div className='flex flex-col items-start gap-3'>
-          <span className='text-gray-950 text-xs font-extrabold bg-yellow-300 py-1 px-3 rounded-sm'>
+          <span className='text-gray-950 text-xs lg:text-sm font-extrabold bg-yellow-300 py-1 px-3 rounded-sm'>
             Learning
           </span>
-          <span className='text-xs text-gray-950 font-medium'>
+          <span className='text-xs lg:text-sm text-gray-950 font-medium'>
             Published 30 Dec 2025
           </span>
-          <h1 className='text-xl text-gray-950 font-extrabold'>
+          <h1 className='text-xl lg:text-2xl text-gray-950 font-extrabold'>
             HTML & CSS foundations
           </h1>
-          <p className='text-sm text-gray-500 font-medium'>
+          <p className='text-sm lg:text-base text-gray-500 font-medium'>
             These languages are the backbone of every website, defining
             structure, content, and presentation.
           </p>
